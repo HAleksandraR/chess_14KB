9942af5959b594b6def80d17e0bb058dc1d6bb78
Дмитрий Баранов <demon.asgard@gmail.com>
2016-09-16 14:22:30 +0500

    Реализована Backbone-модель Figure.

--- chess.js
+++ chess.js
@@ -2,6 +2,22 @@
  * Created by demon on 12.09.16.
  */
 
+var Figure = _.extend(Backbone.Model, {
+
+    defaults: {
+        name: '',
+        text: '',
+        color: '',
+        row: 0,
+        col: 0
+    },
+
+    constructor: function () {
+        Backbone.Model.apply(this, arguments);
+    }
+
+});
+
 $(function() {
     var figures = [];
 
@@ -10,18 +26,18 @@ $(function() {
 
     for(var c = 0; c < 2; c++) {
         for (var i = 0; i < 8; i++) {
-            figures = figures.concat({name: 'pawn', text: '♟', color: colors[c], row: c == 0 ? 1 : 6, col: i});
+            figures = figures.concat(new Figure({name: 'pawn', text: '♟', color: colors[c], row: c == 0 ? 1 : 6, col: i}));
         }
 
         figures = figures.concat([
-            {name: 'rook', text: '♜', color: colors[c], row: rows[c], col: 0},
-            {name: 'knight', text: '♞', color: colors[c], row: rows[c], col: 1},
-            {name: 'bishop', text: '♝', color: colors[c], row: rows[c], col: 2},
-            {name: 'rook', text: '♜', color: colors[c], row: rows[c], col: 7},
-            {name: 'knight', text: '♞', color: colors[c], row: rows[c], col: 6},
-            {name: 'bishop', text: '♝', color: colors[c], row: rows[c], col: 5},
-            {name: 'king', text: '♚', color: colors[c], row: rows[c], col: c == 0 ? 3 : 4},
-            {name: 'queen', text: '♛', color: colors[c], row: rows[c], col: c == 0 ? 4 : 3},
+            new Figure({name: 'rook', text: '♜', color: colors[c], row: rows[c], col: 0}),
+            new Figure({name: 'knight', text: '♞', color: colors[c], row: rows[c], col: 1}),
+            new Figure({name: 'bishop', text: '♝', color: colors[c], row: rows[c], col: 2}),
+            new Figure({name: 'rook', text: '♜', color: colors[c], row: rows[c], col: 7}),
+            new Figure({name: 'knight', text: '♞', color: colors[c], row: rows[c], col: 6}),
+            new Figure({name: 'bishop', text: '♝', color: colors[c], row: rows[c], col: 5}),
+            new Figure({name: 'king', text: '♚', color: colors[c], row: rows[c], col: c == 0 ? 3 : 4}),
+            new Figure({name: 'queen', text: '♛', color: colors[c], row: rows[c], col: c == 0 ? 4 : 3}),
         ]);
     }
 
@@ -32,15 +48,17 @@ $(function() {
             var td = $("<td>");
             var div = $("<div>");
             var f = _.find(figures, function(f) {
-                return f.row == i && f.col == j;
+                return f.get('row') == i && f.get('col') == j;
             });
             if(f) {
-                div.text(f.text);
-                div.addClass(f.color);
+                div.text(f.get('text'));
+                div.addClass(f.get('color'));
                 div.addClass("figure");
+                div.attr({cid: f.cid});
                 $(div).draggable();
                 td.html(div);
             }
+            td.attr({row: i, col: j});
             tr.append(td);
         }
         board.append(tr);
@@ -55,7 +73,19 @@ $(function() {
             el.detach();
             $(this).append(el);
 
+            var cid = el.attr('cid');
+            var f = _.find(figures, function(f) {
+                return f.cid == cid;
+            });
+            f.set({col: $(this).attr('col'), row: $(this).attr('row')});
+
             el.css({left: 0, top: 0});
         }
+    });
+
+    _.each(figures, function(f) {
+        f.on("change:col change:row", function(model, data) {
+            console.log(f.cid, data)
+        });
     });
 });
