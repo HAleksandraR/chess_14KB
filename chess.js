ec9a98624d395bbfea4ac46d3aa9bd2ea0504d3a
Дмитрий Баранов <demon.asgard@gmail.com>
2016-09-16 14:42:23 +0500

    Вывод фигур при их добавлении в коллекцию.

--- chess.js
+++ chess.js
@@ -44,6 +44,48 @@ var Figures = Backbone.Collection.extend({
 $(function() {
     var figures = new Figures();
 
+    var board = $("<table>");
+    for(var i = 0; i < 8; i++) {
+        var tr = $("<tr>");
+        for(var j = 0; j < 8; j++) {
+            var td = $("<td>");
+            var f = figures.find(function(f) {
+                return f.get('row') == i && f.get('col') == j;
+            });
+            td.attr({row: i, col: j});
+            tr.append(td);
+        }
+        board.append(tr);
+    }
+    $("#board").html(board);
+
+    figures.on("add", function(figure) {
+        var v = new FigureView({model: figure});
+        $('#board td[row='+figure.get('row')+'][col='+figure.get('col')+']').html(v.render().el);
+
+        figure.on("change:col change:row", function(model, data) {
+            console.log(figure.cid, data)
+        });
+    });
+
+    $("#board td").droppable({
+        accept: '.figure',
+        drop: function(event, ui) {
+            var el = $(ui.helper[0]);
+
+            el.detach();
+            $(this).append(el);
+
+            var cid = el.attr('cid');
+            var f = figures.get(cid);
+            f.set({col: $(this).attr('col'), row: $(this).attr('row')});
+
+            el.css({left: 0, top: 0});
+        }
+    });
+
+    /////////////////////////////////
+
     var colors = ['black', 'white'];
     var rows = [0, 7];
 
@@ -63,46 +105,4 @@ $(function() {
             {name: 'queen', text: '♛', color: colors[c], row: rows[c], col: c == 0 ? 4 : 3},
         ]);
     }
-
-    var board = $("<table>");
-    for(var i = 0; i < 8; i++) {
-        var tr = $("<tr>");
-        for(var j = 0; j < 8; j++) {
-            var td = $("<td>");
-            var f = figures.find(function(f) {
-                return f.get('row') == i && f.get('col') == j;
-            });
-            if(f) {
-                var v = new FigureView({model: f});
-
-                td.html(v.render().el);
-            }
-            td.attr({row: i, col: j});
-            tr.append(td);
-        }
-        board.append(tr);
-    }
-    $("#board").html(board);
-
-    $("#board td").droppable({
-        accept: '.figure',
-        drop: function(event, ui) {
-            var el = $(ui.helper[0]);
-
-            el.detach();
-            $(this).append(el);
-
-            var cid = el.attr('cid');
-            var f = figures.get(cid);
-            f.set({col: $(this).attr('col'), row: $(this).attr('row')});
-
-            el.css({left: 0, top: 0});
-        }
-    });
-
-    figures.each(function(f) {
-        f.on("change:col change:row", function(model, data) {
-            console.log(f.cid, data)
-        });
-    });
 });
