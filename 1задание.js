35d984e78ff64eb03f111272f34669c179c3e464
HAleksandraR <hakimovaaleksandrar@gmail.com>
2016-10-15 15:18:42 +0300

    Add files via upload

---
+++ 1задание.js
@@ -0,0 +1,24 @@
+<html>
+<head>
+ <title>Шахматы</title>
+</head>
+<body>
+ <canvas id='шахматы'>Обновите страницу</canvas>
+ <script>
+ var example = document.getElementById("шахматы");
+ var ctx = example.getContext('2d');
+ example.width = 300;
+ example.height = 300;
+ ctx.strokeStyle = '#B70A02'; // цвет рамки
+ ctx.strokeRect(15, 15, 266, 266);
+ ctx.strokeRect(18, 18, 260, 260);
+ ctx.fillStyle = '#AF5200'; // цвет клеток
+ ctx.fillRect(20, 20, 256, 256);
+ for (i=0; i<8; i+=2)
+ for (j=0; j<8; j+=2) {
+ ctx.clearRect(20+i*32, 20+j*32, 32, 32);
+ ctx.clearRect(20+(i+1)*32, 20+(j+1)*32, 32, 32);
+ }
+ </script>
+</body>
+<⁄html>
